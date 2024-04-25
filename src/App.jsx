ddff5ee7b3f4a2264bce94825dc29a74ab14d52d
roblebob <robert.koppisch@gmail.com>
2024-04-25 12:06:18 +0200

    Project Details Page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import ProjectListPage from "./pages/ProjectListPage";
 import CreateProjectPage from "./pages/CreateProjectPage";
+import ProjectDetailsPage from "./pages/ProjectDetailsPage";
 
 function App() {
   return (
@@ -12,9 +13,10 @@ function App() {
 
       <Routes>
         <Route path="/" element={<HomePage />} />
-
         <Route path="/projects" element={<ProjectListPage />} />
         <Route path="projects/create" element={<CreateProjectPage />} />
+
+        <Route path="/projects/:projectId" element={<ProjectDetailsPage />} />  
       </Routes>
     </div>
   );
---
+++ src/pages/ProjectDetailsPage.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link, useParams } from "react-router-dom";
+
+const API_URL = "https://project-management-api-4641927fee65.herokuapp.com";
+
+function ProjectDetailsPage(props) {
+  const [project, setProject] = useState(null);
+
+  const { projectId } = useParams();
+
+  const getProject = () => {
+    axios
+      .get(`${API_URL}/projects/${projectId}?_embed=tasks`)
+      .then((response) => setProject(response.data))
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    getProject();
+  }, []);
+
+  return (
+    <div className="ProjectDetailsPage">
+      {project && (
+        <>
+          <h1>{project.title}</h1>
+          <p>{project.description}</p>
+        </>
+      )}
+
+      {project &&
+        project.tasks.map((task) => (
+          <li className="TaskCard card" key={task.id}>
+            <h3>{task.title}</h3>
+            <h4>Description:</h4>
+            <p>{task.description}</p>
+          </li>
+        ))}
+
+      <Link to="/projects">
+        <button>Back to projects</button>
+      </Link>
+
+      {/*    ADD    */}
+      <Link to={`/projects/edit/${projectId}`}>
+        <button>Edit Project</button>
+      </Link>
+    </div>
+  );
+}
+
+export default ProjectDetailsPage;
